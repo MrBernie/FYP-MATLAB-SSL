cf5a3288ff656627f2b262d51a5a39d2074387ad
Bernie <mrfbernardz@gmail.com>
2025-05-23 12:38:14 +0800

    removing realtime matlab

--- realtime-matlab/rir.m
+++
@@ -1,9 +0,0 @@
-% define the Room dimensions
-roomDimensions = [4 4 2.5];
-% define the coordinates of the receiver and transmitter
-receiverCoord = [2 1 1.8];
-sourceCoord = [3 1 1.8];
-% plot the room, receiver, and transmitter
-h = figure;
-plotRoom(roomDimensions,receiverCoord,sourceCoord,h)
-
